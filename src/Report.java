2f23e7b73983d06f7a8e490bc87170f2c2497bc9
Milos194 <milosmmg@gmail.com>
2021-11-08 18:11:44 +0100

    fsa

--- src/Report.java
+++ src/Report.java
@@ -3,18 +3,17 @@ import java.util.List;
 
 public class Report {
 
-    public static String report(Student student, List<Course> course, String reportType, LocalDateTime time){
-        String tempString="";
+    public static String report(Student student, List<Course> course, String reportType, LocalDateTime time) {
+        String tempString = "";
         if (reportType.equals("Short")) {
-            tempString = student.getStudentName() + " ( " +student.getStudentOccupation() +
-                    " ) " + Course.courseReport(Course.courseTimeLeft(course),Course.timeUntilEnd(time));
-        } else if (reportType.equals("Long")){
+            tempString = student.getStudentName() + " ( " + student.getStudentOccupation() +
+                    " ) " + Course.courseReport(Course.courseTimeLeft(course), Course.timeUntilEnd(time));
+        } else if (reportType.equals("Long")) {
 
         }
         return tempString;
     }
 
 
-
-    }
 }
+
